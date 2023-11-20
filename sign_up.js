12cde0d4f7d535645984a4f11fc324624b0f7f75
Julia <121096183+JuliaMaxx@users.noreply.github.com>
2023-11-20 23:14:20 +0100

    feat: make the lable show up only on focus
    Remove placeholder when focused and add a lable

--- sign_up.js
+++ sign_up.js
@@ -1,5 +1,19 @@
+const inputs = document.querySelectorAll('input:not([type="button"])' );
 const pswConfirmation = document.querySelector("#psw-confirmation");
 let valid = false;
+
+
+function addLable(event) {
+    event.target.nextSibling.nextSibling.style.display = "block";
+}
+function removeLable(event) {
+    event.target.nextSibling.nextSibling.style.display = "none";
+}
+inputs.forEach(input => {
+    input.addEventListener('focus', addLable);
+    input.addEventListener('blur', removeLable);
+})
+
 
 function changeBorderColor () {
     pswConfirmation.style.border = valid? "2px solid green": "2px solid red";
@@ -16,13 +30,13 @@ function checkPassword(event) {
     if (event.target.value == password.value && password.checkValidity()){
         valid = true;
         changeBorderColor();
-        console.log('valid')
+        console.log('valid');
     }
     else {
         valid = false;
         changeBorderColor();
-        console.log('invalid')
+        console.log('invalid');
     }
 }
-pswConfirmation.addEventListener("input", checkPassword)
+pswConfirmation.addEventListener("input", checkPassword);
 
