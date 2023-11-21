76fb473e841ff8b511769fa496d6ab7545cba607
Julia <121096183+JuliaMaxx@users.noreply.github.com>
2023-11-21 14:28:15 +0100

    feat: add log in page

---
+++ login.js
@@ -0,0 +1,46 @@
+// lable 
+const inputs = document.querySelectorAll('input:not([type="button"])' );
+
+function addLable(event) {
+    event.target.nextSibling.nextSibling.style.display = "block";
+}
+function removeLable(event) {
+    event.target.nextSibling.nextSibling.style.display = "none";
+}
+
+inputs.forEach(input => {
+    input.addEventListener('focus', (event) => {addLable(event)});
+    input.addEventListener('blur',(event) => {removeLable(event)});
+})
+
+
+// password confirmation
+const pswConfirmation = document.querySelector("#psw-confirmation");
+const password = document.querySelector("#password");
+
+let valid = false;
+
+function changeBorderColor () {
+    pswConfirmation.style.border = valid? "2px solid green": "2px solid red";
+}
+
+pswConfirmation.addEventListener('focus', changeBorderColor);
+pswConfirmation.addEventListener('blur', () => {
+    pswConfirmation.style.border = "none";
+    pswConfirmation.style.borderBottom = "2px solid var(--white)";
+})
+
+function checkPassword(event) {
+    if (event.target.value == password.value && password.checkValidity()){
+        valid = true;
+        changeBorderColor();
+        console.log('valid');
+    }
+    else {
+        valid = false;
+        event.target.setCustomValidity('Two passwords must match')
+        changeBorderColor();
+        console.log('invalid');
+    }
+}
+pswConfirmation.addEventListener("input", checkPassword);
